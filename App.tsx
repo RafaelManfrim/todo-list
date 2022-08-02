0a0313e4bb63f3eb0a34aeaa8ed8a8bb966d012c
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-08-02 12:57:59 -0300

    Iniciando desenvolvimento

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [tasks, setTasks] = useState([])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="transparent" translucent style='light' />
+      <View>
+        <Image source={{ uri: './assets/Logo.svg' }}></Image>
+      </View>
+      <View style={styles.formContainer}>
+        <TextInput placeholder="Adicione uma nova tarefa" placeholderTextColor="#808080" style={styles.input} />
+        <TouchableOpacity style={styles.addButton}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View></View>
     </View>
   );
 }
@@ -13,8 +25,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0D0D0D',
+    paddingHorizontal: 24,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  formContainer: {
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "#262626",
+    padding: 20,
+    borderRadius: 5,
+    marginRight: 4,
+  },
+  addButton: {
+    backgroundColor: "#1E6F9F",
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 18,
+    borderRadius: 6
+  }
 });
